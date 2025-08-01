26bb67437143adc221cad8a9d0eef20b1445bcea
GregoryMiola <reptil9999@gmail.com>
2025-08-01 13:12:30 -0300

    Adjusts about paterns (configs); Fix docker files; Fix migration configs;

--- template/backend/src/Ambev.DeveloperEvaluation.ORM/DefaultContext.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/DefaultContext.cs
@@ -1,4 +1,4 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+﻿﻿using Ambev.DeveloperEvaluation.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
@@ -20,22 +20,23 @@ public class DefaultContext : DbContext
         base.OnModelCreating(modelBuilder);
     }
 }
-public class YourDbContextFactory : IDesignTimeDbContextFactory<DefaultContext>
+public class DefaultContextFactory : IDesignTimeDbContextFactory<DefaultContext>
 {
     public DefaultContext CreateDbContext(string[] args)
     {
-        IConfigurationRoot configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json")
-            .Build();
+        var webApiProjectDir = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\Ambev.DeveloperEvaluation.WebApi"));
+
+        IConfiguration configuration = new ConfigurationBuilder()
+           .SetBasePath(webApiProjectDir)
+           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+           .AddJsonFile($"appsettings.Development.json", optional: true)
+           .AddEnvironmentVariables()
+           .Build();
 
         var builder = new DbContextOptionsBuilder<DefaultContext>();
         var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-        builder.UseNpgsql(
-               connectionString,
-               b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.WebApi")
-        );
+        builder.UseNpgsql(connectionString, b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.ORM"));
 
         return new DefaultContext(builder.Options);
     }
